2940677aad69000acc6e7aac8c8830ed133aa57a
Mauricio Dopazo <mdopazo20@gmail.com>
2023-08-10 21:24:24 -0300

    Solucion

--- eventos.js
+++ eventos.js
@@ -1,5 +1,9 @@
 
 document.getElementsByTagName("div")[0].addEventListener('click', function(){
-    alert("Hola! Soy el div")
+    alert("Hola! Soy el div");
 });
+document.getElementsByTagName("button")[0].addEventListener('click',function(){
+    alert("Hola!");
+    event.stopImmediatePropagation();;
+})
 
